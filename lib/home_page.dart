bf04d2a4d98029b158a3ea064854fc6743c77160
Shahzod Kakhkhorov <57046438+ShahzodK@users.noreply.github.com>
2023-12-06 01:12:33 +0500

    feat: create slider for popular meals

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -25,12 +25,12 @@ class Home_page extends StatelessWidget {
               children: <Widget>[
                 CircleAvatar(
                   radius: 30.0,
-                  backgroundImage: AssetImage('../images/profile-logo.png'), // Use the user's image path here
+                  backgroundImage: AssetImage('../images/profile-logo.png'),
                 ),
                 SizedBox(width: 15.0),
                 Expanded(
                   child: Text(
-                    'Sharof Rashidov street', // Replace with the user's address
+                    'Sharof Rashidov street',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 16.0,
@@ -42,7 +42,84 @@ class Home_page extends StatelessWidget {
               ],
             ),
           ),
-
+          // Padding(
+          //   padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.28),
+          //   child: Container(
+          //     margin: EdgeInsets.only(top: 20.0), // Adjust margin as needed
+          //     decoration: BoxDecoration(
+          //       color: Colors.grey[200],
+          //       borderRadius: BorderRadius.circular(10.0),
+          //     ),
+          //     child: const TextField(
+          //       decoration: InputDecoration(
+          //         prefixIcon: Icon(Icons.search),
+          //         hintText: 'Search...',
+          //         border: InputBorder.none,
+          //         contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          SizedBox(height: 30.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.28),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Popular',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Container(
+                  height: 200.0,
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      height: 200.0,
+                      aspectRatio: 16 / 9,
+                      viewportFraction: 0.5,
+                      initialPage: 0,
+                      enableInfiniteScroll: false,
+                      reverse: false,
+                      autoPlay: true,
+                      autoPlayInterval: Duration(seconds: 310),
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enlargeCenterPage: false,
+                      scrollDirection: Axis.horizontal,
+                    ),
+                    items: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          '../images/popular_carousel_img.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          '../images/popular_carousel_img.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          '../images/popular_carousel_img.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       )
     ),
