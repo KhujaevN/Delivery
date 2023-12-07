97db680ecc9d4f454acfcf7754b49c484a294bef
Shahzod Kakhkhorov <57046438+ShahzodK@users.noreply.github.com>
2023-12-07 23:41:00 +0500

    feat: rewrite header, format code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,123 +6,195 @@ class Home_page extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        color: Color(0xFF181A20),
-        constraints: BoxConstraints(
-          minHeight: MediaQuery.of(context).size.height,
-        ),
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.28),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20.0),
-                bottomRight: Radius.circular(20.0),
+          color: Color(0xFF181A20),
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.28),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 48,
+                      height: 48,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 48,
+                            height: 48,
+                            decoration: const ShapeDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('../images/profile-logo.png'),
+                                fit: BoxFit.fill,
+                              ),
+                              shape: OvalBorder(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              width: double.infinity,
+                              child: Text(
+                                'Delivery Address',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 7),
+                            Container(
+                              width: double.infinity,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Амир Темур, 107Б',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Container(
+                                    width: 20,
+                                    height: 20,
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          width: 20,
+                                          height: 20,
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 5, vertical: 5.83),
+                                          child: const Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            child: const Row(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 30.0,
-                  backgroundImage: AssetImage('../images/profile-logo.png'),
-                ),
-                SizedBox(width: 15.0),
-                Expanded(
-                  child: Text(
-                    'Sharof Rashidov street',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
+              // Padding(
+              //   padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.28),
+              //   child: Container(
+              //     margin: EdgeInsets.only(top: 20.0), // Adjust margin as needed
+              //     decoration: BoxDecoration(
+              //       color: Colors.grey[200],
+              //       borderRadius: BorderRadius.circular(10.0),
+              //     ),
+              //     child: const TextField(
+              //       decoration: InputDecoration(
+              //         prefixIcon: Icon(Icons.search),
+              //         hintText: 'Search...',
+              //         border: InputBorder.none,
+              //         contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              SizedBox(height: 30.0),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.28),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Popular',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.28),
-          //   child: Container(
-          //     margin: EdgeInsets.only(top: 20.0), // Adjust margin as needed
-          //     decoration: BoxDecoration(
-          //       color: Colors.grey[200],
-          //       borderRadius: BorderRadius.circular(10.0),
-          //     ),
-          //     child: const TextField(
-          //       decoration: InputDecoration(
-          //         prefixIcon: Icon(Icons.search),
-          //         hintText: 'Search...',
-          //         border: InputBorder.none,
-          //         contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          SizedBox(height: 30.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.28),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Popular',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Container(
-                  height: 200.0,
-                  child: CarouselSlider(
-                    options: CarouselOptions(
+                    SizedBox(height: 10.0),
+                    Container(
                       height: 200.0,
-                      aspectRatio: 16 / 9,
-                      viewportFraction: 0.5,
-                      initialPage: 0,
-                      enableInfiniteScroll: false,
-                      reverse: false,
-                      autoPlay: true,
-                      autoPlayInterval: Duration(seconds: 310),
-                      autoPlayAnimationDuration: Duration(milliseconds: 800),
-                      autoPlayCurve: Curves.fastOutSlowIn,
-                      enlargeCenterPage: false,
-                      scrollDirection: Axis.horizontal,
+                      child: CarouselSlider(
+                        options: CarouselOptions(
+                          height: 200.0,
+                          aspectRatio: 16 / 9,
+                          viewportFraction: 0.5,
+                          initialPage: 0,
+                          enableInfiniteScroll: false,
+                          reverse: false,
+                          autoPlay: true,
+                          autoPlayInterval: Duration(seconds: 310),
+                          autoPlayAnimationDuration:
+                              Duration(milliseconds: 800),
+                          autoPlayCurve: Curves.fastOutSlowIn,
+                          enlargeCenterPage: false,
+                          scrollDirection: Axis.horizontal,
+                        ),
+                        items: [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              '../images/popular_carousel_img.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              '../images/popular_carousel_img.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              '../images/popular_carousel_img.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    items: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          '../images/popular_carousel_img.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          '../images/popular_carousel_img.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          '../images/popular_carousel_img.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ],
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
-      )
-    ),
+              ),
+            ],
+          )),
     );
   }
 }
